d9074c955ac44d2b040a3150b6d3f7e7f9c9d7c4
apfmiranda <alexandre.miranda@gmail.com>
2019-08-26 13:58:58 -0300

    Recebendo e validando os dados do formulário de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function(app,req, res) {
+
+    var dadosForm = req.body; 
+    req.assert('apelido', 'Nome ou apelido é Obrigatório').notEmpty();
+    req.assert('apelido', 'Minimo de 3 e maximo de 15 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+    console.log(erros);
+    if (erros) {
+        res.render('index', {validacao: erros});
+        return;
+    }   
+
     res.render('chat');
 }
